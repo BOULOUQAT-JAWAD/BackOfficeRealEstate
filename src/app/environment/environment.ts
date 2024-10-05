85d0f162d1fb043e4bb6aab9ea96131b2e3e316a
it-dev-full <nourabouaissa2@gmail.com>
2024-10-05 15:49:56 +0100

    Update environment.ts

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl:"http://localhost:8080/api/",
+  baseUrl:"https://pj-p7uj.onrender.com/api/",
   token: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huLmRvZTFAZXhhbXBsZS5jb20iLCJyb2xlcyI6IltDTElFTlRdIiwiZXhwIjoxNzI1MzA5ODI5fQ.gW-Q3V07WSB3OxLzI3cqcGZNBNweipPLQu2zvXW_g3Y",
 };
